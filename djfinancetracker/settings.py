f8335066d04f4004136c5b1c0e91b8eb44dce193
paka95 <94203043+paka95@users.noreply.github.com>
2022-06-05 03:55:15 -0700

    Update settings.py

--- djfinancetracker/settings.py
+++ djfinancetracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h+)nhy(s&1iyyv#9*sw^%91-*zg=#aa4t1eggu_dm!-2sw6%up'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
